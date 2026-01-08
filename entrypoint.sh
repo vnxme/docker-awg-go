672e0602536d1c04fbbbfc7150511521969371b7
vnxme <46669194+vnxme@users.noreply.github.com>
2026-01-08 14:37:37 +0300

    Delete redundant S

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,28 +2,28 @@
 
 # Supported environment variables:
 #
-# VARIABLE   # DESCRIPTION                                 # DEFAULT VALUE          #
-# ---------- # ------------------------------------------- # ---------------------- #
-# CONFS_DEF  # the default interface name                  # wg0                    #
-# CONFS_DIR  # the configuration directory                 # /etc/amnezia/amneziawg #
-# CONFS_LIST # the comma-separated list of interface names # ${CONFS_DEF}           #
-# HOOKS_DIR  # the pre/post-up/down hook scripts directory # ./hooks                #
+# VARIABLE  # DESCRIPTION                                 # DEFAULT VALUE          #
+# --------- # ------------------------------------------- # ---------------------- #
+# CONF_DEF  # the default interface name                  # wg0                    #
+# CONF_DIR  # the configuration directory                 # /etc/amnezia/amneziawg #
+# CONF_LIST # the comma-separated list of interface names # ${CONF_DEF}            #
+# HOOK_DIR  # the pre/post-up/down hook scripts directory # ./hooks                #
 #
-# Note: if ${CONFS_DIR} exists and no non-empty configuration files are found in it
-# according to ${CONFS_LIST}, the script runs the daemon with each non-empty *.conf
+# Note: if ${CONF_DIR} exists and no non-empty configuration files are found in it
+# according to ${CONF_LIST}, the script runs the daemon with each non-empty *.conf
 # file. The script creates a new configuration if there are no *.conf files at all.
 
-CONFS_DEF="${CONFS_DEF:-wg0}"
-CONFS_DIR="${CONFS_DIR:-/etc/amnezia/amneziawg}"
-CONFS_LIST="${CONFS_LIST:-${CONFS_DEF}}"
-HOOKS_DIR="${HOOKS_DIR:-./hooks}"
+CONF_DEF="${CONF_DEF:-wg0}"
+CONF_DIR="${CONF_DIR:-/etc/amnezia/amneziawg}"
+CONF_LIST="${CONF_LIST:-${CONF_DEF}}"
+HOOK_DIR="${HOOK_DIR:-./hooks}"
 
 PIDS=()
 FILES=()
 
 hooks() {
-	[ ! -d "${HOOKS_DIR}/$1" ] && mkdir -p "${HOOKS_DIR}/$1"
-	local FILE; for FILE in "${HOOKS_DIR}"/"$1"/*.sh; do
+	[ ! -d "${HOOK_DIR}/$1" ] && mkdir -p "${HOOK_DIR}/$1"
+	local FILE; for FILE in "${HOOK_DIR}"/"$1"/*.sh; do
 		if [ -s "${FILE}" ]; then
 			/bin/bash -- "${FILE}" || true
 		fi
@@ -36,9 +36,9 @@ launch() {
 
 	# Launch one or multiple tunnels
 	if [ -n "$(which awg)" ] && [ -n "$(which awg-quick)" ] && [ -n "$(which amneziawg-go)" ]; then
-		local CONFS; IFS=',' read -r -a CONFS <<< "${CONFS_LIST}"
+		local CONFS; IFS=',' read -r -a CONFS <<< "${CONF_LIST}"
 		local CONF; for CONF in "${CONFS[@]}"; do
-			local FILE="${CONFS_DIR}/${CONF}.conf"
+			local FILE="${CONF_DIR}/${CONF}.conf"
 			if [ -s "${FILE}" ]; then
 				awg-quick down "${FILE}" || true
 				awg-quick up "${FILE}" || true
@@ -46,12 +46,12 @@ launch() {
 			fi
 		done
 
-		if [ ${#FILES[@]} -eq 0 ] && [ -d "${CONFS_DIR}" ]; then
-			if [ -n "$(find "${CONFS_DIR}" -maxdepth 0 -type d -empty)" ]; then
-				cd -- "${CONFS_DIR}" && bash -- /app/configure.sh "${CONFS_DEF}" && cd -
+		if [ ${#FILES[@]} -eq 0 ] && [ -d "${CONF_DIR}" ]; then
+			if [ -n "$(find "${CONF_DIR}" -maxdepth 0 -type d -empty)" ]; then
+				cd -- "${CONF_DIR}" && bash -- /app/configure.sh "${CONF_DEF}" && cd -
 			fi
 
-			local FILE; for FILE in "${CONFS_DIR}"/*.conf; do
+			local FILE; for FILE in "${CONF_DIR}"/*.conf; do
 				if [ -s "${FILE}" ]; then
 					awg-quick down "${FILE}" || true
 					awg-quick up "${FILE}" || true
