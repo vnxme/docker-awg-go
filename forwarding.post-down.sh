3b6647f18866cd5ee28b23147c89646eb57d6bdd
vnxme <46669194+vnxme@users.noreply.github.com>
2026-01-09 23:50:38 +0300

    Delete redundant scripts

--- forwarding.post-down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "${FORWARDING_IPV4}" > /proc/sys/net/ipv4/ip_forward
-echo "${FORWARDING_IPV6_ALL}" > /proc/sys/net/ipv6/conf/all/forwarding
-echo "${FORWARDING_IPV6_DEF}" > /proc/sys/net/ipv6/conf/default/forwarding
